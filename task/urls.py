1c900065e999001508665110778195390cf98a46
Vlad <vlad@exampl.com>
2026-05-08 15:35:46 +0300

    created TaskDetailView, TaskListView with filters

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, TaskListView, TaskCreateView
 
 app_name = 'task'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('tasks/', TaskListView.as_view(), name="task-list"),
+    path('tasks/create/', TaskCreateView.as_view(), name="task-create"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse, reverse_lazy
+
+from .models import Task
 
 # Create your views here.
 def index(request):
     return render(request, "task/index.html")
+
+class TaskListView(generic.ListView):
+    model = Task
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        req = self.request.GET
+        completed = req.get("completed")
+        if completed is not None:
+            queryset = queryset.filter(is_completed=completed == "true")
+        if req.get("my"):
+            user = self.request.user
+            queryset = queryset.filter(assignees=user)
+        return queryset
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = ["name", "description", "deadline", "priority",
+              "task_type", "assignees"]
+    success_url = reverse_lazy("task:task-list")
