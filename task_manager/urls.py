2f8933e655fe28a9c5e4c4fd17616345864990e8
Vlad <vlad.grinkiv@stud.onu.edu.ua>
2026-06-01 22:51:44 +0300

    changed urls.py so that django could give media file

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('task.urls', namespace='task')),
-    path('accounts/', include('django.contrib.auth.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
